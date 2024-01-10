48fce996b4197829ab25fe512aeaffc0dfa5822d
David Adeogun <adeogundavidt@gmail.com>
2024-01-09 19:00:23 -0500

    first commit

---
+++ src/TodoList.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import { TodoItem } from "./TodoItem"
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo => {
+        return (
+          <TodoItem
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        )
+      })}
+    </ul>
+  )
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      title: PropTypes.string,
+      completed: PropTypes.bool
+    })
+  ).isRequired,
+  toggleTodo: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired,
+};
